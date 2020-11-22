7baa9ba29e76ec467632d482735facdf8d48f7b0
Chang Liao <changliao1025@gmail.com>
2020-11-22 11:19:05 -0800

    bug

--- docs/doxygen/html/search/variables_64.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['dstefan_5fboltzmann',['dStefan_boltzmann',['../global_8cpp.html#a787a5bf089f9f792fdd0a37254f40b9c',1,'dStefan_boltzmann():&#160;global.cpp'],['../global_8h.html#a787a5bf089f9f792fdd0a37254f40b9c',1,'dStefan_boltzmann():&#160;global.cpp']]]
-];
